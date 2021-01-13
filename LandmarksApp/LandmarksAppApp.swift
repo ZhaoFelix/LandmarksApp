4a06bfe71bcc90de4f6fc7638454b6bc62337014
zhaofelix <15021179915@163.com>
2021-01-13 10:11:53 +0800

    feat:初始化项目

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by FelixZhao on 2021/1/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

e8727463f53d052ebae82ec89bbd0ffb191c7829
zhaofelix <15021179915@163.com>
2021-01-13 10:59:39 +0800

    第一章节完成

---
+++ LandmarksApp/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  LandmarksApp
+//
+//  Created by FelixZhao on 2021/1/13.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("img")
+            .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray,lineWidth: 4))
+            .shadow(radius: 7)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}

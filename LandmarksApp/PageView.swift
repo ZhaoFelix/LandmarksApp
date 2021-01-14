9491138c8b0dc280b12823621af79254fed01255
zhaofelix <15021179915@163.com>
2021-01-14 13:20:09 +0800

    SwiftUI中引入UIKit

--- LandmarksApp/PageView.swift
+++ LandmarksApp/PageView.swift
@@ -11,7 +11,12 @@ struct PageView<Page:View>: View {
     var pages:[Page]
     @State private var currentPage = 0
     var body: some View {
-        PageViewController(pages: pages,currentPage:$currentPage)
+        ZStack(alignment:.bottomTrailing) {
+            PageViewController(pages: pages,currentPage:$currentPage)
+            PageControl(numberOfPages:pages.count,currentPage:$currentPage)
+                .frame(width:CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
     }
 }
 
